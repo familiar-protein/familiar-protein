e24e08220472c2f8aa784a614ba43447893fabc6
Emily Eng <emily.w.eng@gmail.com>
2015-08-05 16:31:22 -0700

    Filled out database calls for question controller.

--- server/questions/questionController.js
+++ server/questions/questionController.js
@@ -1,14 +1,23 @@
+var Question = require('./questionModel');
+
 // adds a question to the database
 var add = function(req, res, next) {
 
   console.log(req.body);
   var question = {
+    qNumber: req.body.qNumber,
     title: req.body.title,
     description: req.body.description
   }
 
-  // TODO add question to database
-  res.json(question);
+  var newQ = new Question(question);
+  newQ.save(function(err, newEntry) {
+    if (err) {
+      res.send(500, err);
+    } else {
+      res.send(200, newEntry);
+    }
+  });
 };
 
 
@@ -16,8 +25,13 @@ var add = function(req, res, next) {
 var getAll = function(req, res, next) {
 
   // TODO fetch question data
-
-  res.json({test: 'test'});
+  Question.find().exec(function(err, data) {
+    if (err) {
+      res.send(500, err);
+    } else {
+      res.json(data);
+    }
+  });
 };
 
 
