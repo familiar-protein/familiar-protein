45bd0c36edffa989cc9c5cf4658589dc382d4dc4
Emily Eng <emily.w.eng@gmail.com>
2015-08-05 19:25:34 -0700

    Hardcode some initial DB data for now.

--- server/questions/questionModel.js
+++ server/questions/questionModel.js
@@ -7,5 +7,26 @@ var QuestionSchema = new mongoose.Schema({
   // examples?
 });
 
-module.exports = mongoose.model('Question', QuestionSchema);
+var Question = mongoose.model('Question', QuestionSchema);
+
+// Set up some dummy initial data for now by hardcoding.
+var questions = [
+  {qNumber: 1, title: "Capital Words", description: "Find all capital words in a string." },
+  {qNumber: 2, title: "Does a Word Contain One 'a'", description: "Validate whether the given word contains one and only one letter 'a'."},
+  {qNumber: 3, title: "Find Prices", description: "Find all prices in the string."}
+]
+
+questions.forEach(function(element) {
+  var newQ = new Question(element);
+  newQ.save(function(err, data) {
+    if (err) {
+      console.log("Adding questions error: ", err);
+    } else {
+      console.log("Adding questions success: ", data);
+    }
+  });
+});
+
+
+module.exports = Question;
 
