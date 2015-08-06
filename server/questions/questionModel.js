ede030fd7db1051d1cc740698acb376a41261b60
jan <jlhellmich@gmail.com>
2015-08-06 12:07:52 -0700

    Add question test data to database

--- server/questions/questionModel.js
+++ server/questions/questionModel.js
@@ -16,9 +16,9 @@ var questions = [
   {
     qNumber: 1,
     title: "Capital Words",
-    description: "Find all capital words in a string.",
-    truthy: [],
-    falsy: []
+    description: "Validate whether a given string starts with a capital letter",
+    truthy: ['Erik', 'PC', 'Microsoft', 'Hack Reactor', 'London', 'Alabama'],
+    falsy: ['table', 'computer', 'hello', 'hEllo', '$Hello']
   },
   {
     qNumber: 2,
@@ -30,10 +30,25 @@ var questions = [
   {
     qNumber: 3,
     title: "Find Prices",
-    description: "Find all prices in the string.",
-    truthy: [],
-    falsy: []
+    description: "Determine whether the given string is a valid price",
+    truthy: ['$123', '$0', '$0.50', '$99.99', '$102472349.17'],
+    falsy: ['hello', '123', '$0.577', '$123.4.3', '$823.h']
+  },
+  {
+    qNumber: 4,
+    title: "Username Validation",
+    description: "Determine whether a given username is valid according to the following restrictions: A username has to be between 6 and 10 characters and can only contain alphanumeric characters and '_'!",
+    truthy: ['myname', 'ingikim', 'ingi_kim', 'alpha123'],
+    falsy: ['myreallylongname', '@symbol', 'ingi-kim', 'name', 'jfah&kf']
+  },
+  {
+    qNumber: 5,
+    title: "Hex Color Code",
+    description: "Determine whether a given string is valid Hex color code.",
+    truthy: ['#FFF', '#abcabc', '#123', '#4d2', '#83e9db'],
+    falsy: ['#ffff', 'fff', '123123', '%FFF', '#897f', '#44444444']
   }
+
 ];
 
 questions.forEach(function(element) {
