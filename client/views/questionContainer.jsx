8a593cb03f3ebe64c51e32ff19d171349e87b4d1
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-07 15:37:45 -0700

    iFlag is set with button, regex input sandwiched

--- client/views/questionContainer.jsx
+++ client/views/questionContainer.jsx
@@ -1,11 +1,12 @@
 var QuestionContainer = React.createClass({
   getInitialState: function(){
-    return {result: ''};
+    return {result: '', iFlag: false};
   },
   submit: function(e){
     e.preventDefault();
     console.log(this);
-    var iFlag = React.findDOMNode(this.refs.iFlag).value;
+    var iFlag = (this.state.iFlag === true) ? 'i' : '';
+    console.log(iFlag);
     var answer = React.findDOMNode(this.refs.solutionText).value;
     var payload = JSON.stringify({regexString: answer, iFlag: iFlag});
     $.ajax({
@@ -31,12 +32,9 @@ var QuestionContainer = React.createClass({
           <div id="result">{this.state.result}</div>
           <form name="questionSolution" >
             <div className="form-group">
-              <label for="iFlag">iFlag</label>
-              <input placeholder="iFlag" className=".form-control" type="text" id="iFlag" ref="iFlag" />
-            </div>
-            <div className="form-group">
               <label for="solution">Solution Regex</label>
-              <textarea placeholder="Regex solution..." classsName=".form-control" id="solution" ref="solutionText"></textarea>
+              /<input placeholder="Regex solution..." classsName=".form-control" id="solution" ref="solutionText"></input>/
+              <button onClick={function(){this.setState({iFlag: !this.state.iFlag, questions: this.state.questions})}.bind(this)} className="btn" >iFlag</button>
             </div>
             <button onClick={this.submit} className="btn btn-primary" name="solutionButton">Check Answer!</button>
           </form>
