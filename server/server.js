a58a1666941526aadc6b90ca548b87b6a45be250
jan <jlhellmich@gmail.com>
2015-08-07 11:32:22 -0700

    Changed db to handle testing database, start basic testing

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ var app = express();
 
 middleware(app);
 
-app.listen(port);
+// only execute if server.js was the root process rather than being required by other module,
+// for example, for access in test suite.
+// if (!module.parent) {
+  app.listen(port);
+  console.log('Server now listening on port ' + port);
+// }
 
-console.log('Server now listening on port ' + port);
+module.exports = app;
 
