cd5eea2d2dcb6b7e26f8420dbe9e9fa5122492e7
MichaelGofron <gofronm@yahoo.com>
2015-08-19 16:25:22 -0700

    Got rid of unnecessary comments

--- client/src/views/MultiplayerViews/JoinGameView.jsx
+++ client/src/views/MultiplayerViews/JoinGameView.jsx
@@ -8,17 +8,6 @@ var JoinGameView = React.createClass({
   
   mixins: [Navigation, Router.State],
 
-  // createGame: function(){
-  //   console.log("Creating Game");
-  //   // navigate to Create Game with code
-
-  // },
-
-  // joinGame: function(){
-  //   console.log("Joining Game");
-  //   // navigate to join game with code and name
-  // },
-
   render: function(){
     return (
       <div>
