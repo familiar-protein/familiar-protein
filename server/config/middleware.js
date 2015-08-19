4a53ec507e8280774b50fc4cf906142427109aa1
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-19 11:14:45 -0700

    (feat)setting up github auth w/passport

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,14 +1,30 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var userRoutes = require('../users/userRoutes');
 var questionRoutes = require('../questions/questionRoutes');
+var authRoutes = require('../auth/authRoutes');
+
+var passport = require('passport');
+var GithubStrategy = require('passport-github').Strategy;
+
+var GITHUB_CLIENT_ID = 'e47b0909a724130cfdac';
+var GITHUB_CLIENT_SECRET = 'd3df248c72cdbcaeef66038939d992cf959f7189';
 
 module.exports = function(app) {
   app.use(bodyParser.json());
-
+  app.use(express.cookieParser());
   app.use(express.static(__dirname + '/../../client/'));
+  app.use(express.session({secret: 'keyboard cat'}));
 
-  // userRoutes(app);
+  // pasport initialization
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
+  // routing
+  authRoutes(app);
+  userRoutes(app);
   questionRoutes(app);
 };
 
