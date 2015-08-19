c56dc3d1eabdb247445c3a814d8a047db18968a0
SARA GUDEMAN <sgudeman982@gmail.com>
2015-08-18 20:10:50 -0700

    (feat)adds in user routes & user controller (w/out user auth)

---
+++ server/users/userController.js
@@ -0,0 +1,34 @@
+var User = require('./userModel');
+
+
+var signup = function() {};
+
+var login = function() {};
+
+var logout = function() {};
+
+var getUserData = function(req, res, next, id) {
+  User.findOne({_id: id}).exec(function(err, data) {
+    if (err) {
+      res.send(500, err);
+    } else {
+      req.userData = data;
+      next();
+    }
+  });
+};
+
+var userProfile = function(req, res, next) {
+  res.status(200);
+  res.send(req.userData);
+};
+
+var getAllUsers = function(req, res, next) {
+  User.find().exec(function(err, data) {
+    if (err) {
+      res.send(500, err);
+    } else {
+      res.json(data);
+    }
+  });
+};
