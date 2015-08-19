1b610d109bf5f23e6416d0322f6cf6aa87749e72
johndrkurtcom <john@drkurt.com>
2015-08-19 15:26:32 -0700

    (googleAuth) fixed bug in questionRoutes

--- server/questions/questionRoutes.js
+++ server/questions/questionRoutes.js
@@ -2,9 +2,9 @@ var questionController = require('./questionController.js');
 var utils = require('../config/authHelper');
 
 module.exports = function (app) {
-  app.post('/questions', isAuth, questionController.add);
+  app.post('/questions', utils.isAuth, questionController.add);
   app.get('/questions', questionController.getAll);
-  // app.get('/questions', isAuth, questionController.getAll);
+  // app.get('/questions', utils.isAuth, questionController.getAll);
 
   app.param('id', questionController.getQuestionData);
 
