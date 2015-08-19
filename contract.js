0ddd418beeb15da6d53e1121d3b9892c558c28e2
johndrkurtcom <john@drkurt.com>
2015-08-19 10:44:52 -0700

    (contract) initial contract for submitting questions and requesting user info

---
+++ contract.js
@@ -0,0 +1,28 @@
+user = {
+  google: {
+    id: hex, 
+    token: hex,
+    username: name,
+    pic: url
+  },
+
+  score: number,
+
+  stats: [
+    {
+      question: bla bla,
+      score: number,
+      time: number
+    }
+  ]
+}
+
+question = {
+  question: {
+    title: "Capital Words",
+    description: "Validate whether a given string starts with a capital letter",
+    truthy: ['Home', 'PC', 'Microsoft', 'Hack Reactor', 'London'],
+    falsy: ['table', 'computer', 'hello', 'hEllo', '$Hello'],
+    solution: "/kdsjf;/g"
+  }
+}
