a2dd9061d592e9ca8730f975852bbbe95cb069a3
MichaelGofron <gofronm@yahoo.com>
2015-08-19 19:39:31 -0700

    General cleanup

--- server/game/gameController.js
+++ server/game/gameController.js
@@ -1,18 +1,12 @@
 var Game = require('./gameModel');
 
 var makeGame = function(req, res, next){
-  
-  // will want to add questions to the game such that it is no longer
-  // simply an empty object
-  console.log("makeGame in controller");
 
   var game = {
     _id: req.body.id,
     players: [],
     questions: [1,2,3,4,5,6,7] // hard coded question numbers
   };
-
-  console.log("Making new Game in gameController");
 
   var newGame = new Game(game);
   newGame.save(function(err, newEntry){
