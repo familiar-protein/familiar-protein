3cd2abf951ecd6dd3536a8a7a3c1830754f8ba83
johndrkurtcom <john@drkurt.com>
2015-08-20 15:21:32 -0700

    (questions) added seed questions back into database

--- server/questions/questionModel.js
+++ server/questions/questionModel.js
@@ -13,17 +13,17 @@ var QuestionSchema = new mongoose.Schema({
 
 var Question = mongoose.model('Question', QuestionSchema);
 
-// Set up some dummy initial data for now by hardcoding.
-// questions.forEach(function(element) {
-//   var newQ = new Question(element);
-//   newQ.save(function(err, data) {
-//     if (err) {
-//       console.log("Adding questions error: ", err);
-//     } else {
-//       console.log("Adding questions success: ", data);
-//     }
-//   });
-// });
+Set up some dummy initial data for now by hardcoding.
+questions.forEach(function(element) {
+  var newQ = new Question(element);
+  newQ.save(function(err, data) {
+    if (err) {
+      console.log("Adding questions error: ", err);
+    } else {
+      console.log("Adding questions success: ", data);
+    }
+  });
+});
 
 
 module.exports = Question;
